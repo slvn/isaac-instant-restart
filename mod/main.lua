db42a69c3ead06bd58c7575c9a66fd194d0011c2
Sylvain Galand <sylvain.galand@gmail.com>
2021-05-31 17:26:32 -0700

    Initial commit

---
+++ mod/main.lua
@@ -0,0 +1,35 @@
+
+local ____modules = {}
+local ____moduleCache = {}
+local ____originalRequire = require
+local function require(file)
+    if ____moduleCache[file] then
+        return ____moduleCache[file]
+    end
+    if ____modules[file] then
+        ____moduleCache[file] = ____modules[file]()
+        return ____moduleCache[file]
+    else
+        if ____originalRequire then
+            return ____originalRequire(file)
+        else
+            error("module '" .. file .. "' not found")
+        end
+    end
+end
+____modules = {
+["main"] = function() --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+mod = RegisterMod("Instant Restart", 1)
+function inputAction(self, _entity, _hook, buttonAction)
+    if (Input.IsActionTriggered(ButtonAction.ACTION_RESTART, 0) and (buttonAction == ButtonAction.ACTION_RESTART)) and (not Game():IsPaused()) then
+        Isaac.DebugString("----- instant-restart - Restart -----  ")
+        Isaac.ExecuteCommand("restart")
+        return false
+    end
+    return nil
+end
+mod:AddCallback(ModCallbacks.MC_INPUT_ACTION, inputAction, InputHook.IS_ACTION_PRESSED)
+Isaac.DebugString("instant-restart initialized.")
+end,
+}
+return require("main")
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+// Register the mod
+const mod = RegisterMod("Instant Restart", 1);
+
+// Define callback functions
+function inputAction(
+  _entity: Entity,
+  _hook: any,
+  buttonAction: ButtonAction
+) : number | boolean | null {
+  if (
+    Input.IsActionTriggered(ButtonAction.ACTION_RESTART, 0)
+      && buttonAction == ButtonAction.ACTION_RESTART
+      && !Game().IsPaused()
+  ) {
+    Isaac.DebugString("----- instant-restart - Restart -----  ")
+    Isaac.ExecuteCommand("restart")
+    return false
+  }
+  return null
+}
+
+// Register callbacks
+mod.AddCallback(ModCallbacks.MC_INPUT_ACTION, inputAction, InputHook.IS_ACTION_PRESSED);
+
+// Print an initialization message to the "log.txt" file
+Isaac.DebugString("instant-restart initialized.");
